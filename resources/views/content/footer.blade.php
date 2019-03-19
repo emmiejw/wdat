61dba807633e872d7d95503a2968bcd7217d6ade
emmiejw <emilyprice84@outlook.com>
2019-03-19 21:25:01 +0000

    footer editted

--- resources/views/content/footer.blade.php
+++ resources/views/content/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer">
     <div class="container">
         <div class="row">
-        <div class="col l6 s12">
+        <div class="col l12 s12">
             <div class="container">
                 © 2019 Copyright Workplace Drug & Alcohol Testing  
                 <br>
@@ -9,14 +9,7 @@
                 <br>
                 Website Designed and Created by <a href="https://emilywallace-price.co.uk">Emily Wallace - Price </a> 
             </div>
-        </div>
-        <div class="col l4 offset-l2 s12">
-            <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">What we Do</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">About Us</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a></li>
-            </ul>
-        </div>
+            <br>
         </div>
     </div>
 </footer>
