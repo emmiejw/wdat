0eb68235c1521d609b8b07d1b8c39c4b63ac5c37
emmiejw <emilyprice84@outlook.com>
2019-05-05 13:02:37 +0100

    navbar made fixed colour

--- resources/views/content/nav.blade.php
+++ resources/views/content/nav.blade.php
@@ -1,20 +1,22 @@
-<div class="navbar-fixed">
-    <nav class="nav-wrapper #e8f5e9 green lighten-5">
+    <div class="navbar-fixed">
+<nav class="nav-wrapper #e8f5e9 green lighten-5">
         <div class="container">
-            <a href="#" class="sidenav-trigger  brand-logo center" data-target="mobile-menu" style="color:indigo;"><img src="images/logofinal2.png" style="height:50px; width:150px; ">
-            </a>
-            <a href="#" class="sidenav-trigger" data-target="mobile-menu" style="color:indigo;">
-                    <i class="large material-icons left" style="color:indigo;">menu</i>
+            <a href="#" class="sidenav-trigger" data-target="mobile-menu">
+            <i class="large material-icons" style="color:indigo;">menu</i>
             </a>
             <ul class="left hide-on-med-and-down">
             <li><a style="color:indigo;"href="#about">About Us</a></li>
             <li><a style="color:indigo;"href="#services">Services</a></li>
             <li><a style="color:indigo;"href="#contact">Contact</a></li>
             </ul>
-            <ul class="sidenav grey lighten-2" id="mobile-menu">
+            
+            <img src="images/logofinal2.png" class="brand-logo center" style="height:50px; width:100;">
+        </div>
+    </nav>
+    </div>
+    <ul class="sidenav grey lighten-2" id="mobile-menu">
             <li><a style="color:indigo;"href="#about">About Us</a></li>
             <li><a style="color:indigo;"href="#services">Services</a></li>
             <li><a style="color:indigo;"href="#contact">Contact</a></li>
             </ul>
-        </div>
-    </nav>
+    
