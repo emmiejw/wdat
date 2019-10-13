9acfd9f761f8a038ee871153678247d15d11621e
emmiejw <emilyprice84@outlook.com>
2019-10-13 15:00:11 +0100

    added mylivechat script tag .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -162,9 +162,8 @@
       $('.tooltipped').tooltip();
       $('.scrollspy').scrollSpy();
     });
-    <script type="text/javascript">function add_chatinline(){var hccid=63063862;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
-add_chatinline(); </script>
-    <!--   script(src='//cdn.transifex.com/live.js')-->
   </script>
+   <script type="text/javascript">function add_chatinline(){var hccid=63063862;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
+    add_chatinline(); </script>
 </body>
 </html>
