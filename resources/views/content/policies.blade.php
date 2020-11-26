d053d43c42c4908a6d57d5452bf7a718ddbcae16
emmiejw <emilyprice84@outlook.com>
2020-11-26 19:06:26 +0000

    made policies cols responsive

--- resources/views/content/policies.blade.php
+++ resources/views/content/policies.blade.php
@@ -3,16 +3,16 @@
             <h2>Our Policies</h2>
             <p>Please take a look at our policies and if you have an queries, please do get in touch.</p>
             <div class="row" style=" display: inline;">
-                <div class="col s3">
+                <div class="col s12 m12 l3">
                         <a href="policies/WDAT Environmental Policy.docx">WDAT Environmental Policy</a>
                 </div>
-                <div class="col s3">
+                <div class="col s12 m12 l3">
                         <a href="policies/WDAT GDPR Policy.docx">WDAT GDPR Policy</a>
                 </div>
-                <div class="col s3">
+                <div class="col s12 m12 l3">
                         <a href="policies/WDAT Waste Management Policy.docx">WDAT Waste Management Policy</a>
                 </div>
-                <div class="col s3">
+                <div class="col s12 m12 l3">
                         <a href="policies/WDAT GDPR Covid19 Policy.docx">WDAT GDPR Covid19 Policy</a>
             </div>
         </div>
